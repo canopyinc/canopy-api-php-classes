20524a55b7b8405901ed44dd2ab962dbed220869
kcleveland <kyle@nerdphone.net>
2014-03-06 15:08:15 -0700

    initial commit

---
+++ Account.php
@@ -0,0 +1,65 @@
+<?php
+
+class Account {
+    
+    var $request_result;
+    
+    
+    function put($system_realm,$parent_account_id,$data,$auth_token) {    
+                                                                                 
+    $ch = curl_init('http://' . $system_realm . ':8000/v2/accounts/' . $parent_account_id);                                                                      
+          curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");                                                                    
+          curl_setopt($ch, CURLOPT_POSTFIELDS, $data);                                                                 
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                     
+          curl_setopt($ch, CURLOPT_HTTPHEADER, array(  
+          'X-Auth-Token: '.$auth_token,
+          'Content-Length: '.strlen($data),                                                              
+          'Content-Type: application/json'                                                                   
+          ));                                                                                                                  
+
+    $result = curl_exec($ch);
+    $result_json_decoded = json_decode($result);
+  
+    return $this->request_result = $result_json_decoded;
+    }
+
+
+    function get($system_realm,$account_id,$auth_token) {
+                                                                           
+    $ch = curl_init('http://' . $system_realm . ':8000/v2/accounts/' . $account_id);                                                                    
+          curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");                                                                                                                                   
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                     
+          curl_setopt($ch, CURLOPT_HTTPHEADER, array(  
+          'X-Auth-Token: '.$auth_token,                                                             
+          'Content-Type: application/json'                                                                   
+          ));                                                                                                                  
+
+    $result = curl_exec($ch);
+    $result_json_decoded = json_decode($result);
+    
+    return $this->request_result = $result_json_decoded;
+    }
+
+
+    function delete($system_realm,$account_id_to_remove,$auth_token) {
+        
+    $ch = curl_init('http://' . $system_realm . ':8000/v2/accounts/' . $account_id_to_remove);                                                                    
+          curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");                                                                                                                                   
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                     
+          curl_setopt($ch, CURLOPT_HTTPHEADER, array(  
+          'X-Auth-Token: '.$auth_token,                                                             
+          'Content-Type: application/json'                                                                   
+          ));                                                                                                                  
+
+    $result = curl_exec($ch);
+    $result_json_decoded = json_decode($result);
+    
+    return $this->request_result = $result_json_decoded;
+        
+    }
+
+    //TODO: function update_account
+    
+}
+
+?>
---
+++ GenerateAuthToken.php
@@ -0,0 +1,30 @@
+<?php
+
+class GenerateAuthToken {
+    
+    var $request_result;
+    
+    function get_auth_token_by_hash($system_realm,$username,$password) {
+      
+    $user_creds = ''.$username.':'.$password.'';
+    $user_creds_to_hash = md5($user_creds);
+    $data = '{"data":{"credentials": "' . $user_creds_to_hash . '","realm": "' . $system_realm . '"}}';
+                                                                                                                                          
+    $ch = curl_init('http://' . $system_realm . ':8000/v2/user_auth'); //the URI to call                                                                     
+          curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");                                                                    
+          curl_setopt($ch, CURLOPT_POSTFIELDS, $data);                                                                 
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                     
+          curl_setopt($ch, CURLOPT_HTTPHEADER, array(  
+          'Content-Length: '.strlen($data),                                                              
+          'Content-Type: application/json'                                                                   
+          ));                                                                                                                  
+
+    $result = curl_exec($ch);
+    $result_json_decoded = json_decode($result);
+    
+    return $this->request_result = $result_json_decoded;
+  
+    }  
+}
+
+?>
