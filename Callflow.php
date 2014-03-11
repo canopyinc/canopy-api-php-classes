ff35119c85f2e4d0f76e21411455b48a2bd49c95
kcleveland <kyle@nerdphone.net>
2014-03-11 07:20:07 -0600

    added class Callflow.php

---
+++ Callflow.php
@@ -0,0 +1,99 @@
+<?php
+
+class Callflow {
+    
+    var $request_result;
+    
+    
+    function put($system_realm,$account_id,$data,$auth_token) {    
+                                                                                 
+    $ch = curl_init('http://' . $system_realm . ':8000/v2/accounts/' . $account_id . '/callflows');                                                                      
+          curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");                                                                    
+          curl_setopt($ch, CURLOPT_POSTFIELDS, $data);                                                                 
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                     
+          curl_setopt($ch, CURLOPT_HTTPHEADER, array(  
+          'X-Auth-Token: '.$auth_token,
+          'Content-Length: '.strlen($data),                                                              
+          'Content-Type: application/json'                                                                   
+          ));                                                                                                                  
+
+    $result = curl_exec($ch);
+    $result_json_decoded = json_decode($result);
+  
+    return $this->request_result = $result_json_decoded;
+    }
+
+
+    function get_all($system_realm,$account_id,$auth_token) {
+                                                                           
+    $ch = curl_init('http://' . $system_realm . ':8000/v2/accounts/' . $account_id . '/callflows');                                                                    
+          curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");                                                                                                                                   
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                     
+          curl_setopt($ch, CURLOPT_HTTPHEADER, array(  
+          'X-Auth-Token: '.$auth_token,                                                             
+          'Content-Type: application/json'                                                                   
+          ));                                                                                                                  
+
+    $result = curl_exec($ch);
+    $result_json_decoded = json_decode($result);
+    
+    return $this->request_result = $result_json_decoded;
+    }
+
+    
+    function get($system_realm,$account_id,$callflow_id,$auth_token) {
+                                                                           
+    $ch = curl_init('http://' . $system_realm . ':8000/v2/accounts/' . $account_id . '/callflows' . $callflow_id);                                                                    
+          curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");                                                                                                                                   
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                     
+          curl_setopt($ch, CURLOPT_HTTPHEADER, array(  
+          'X-Auth-Token: '.$auth_token,                                                             
+          'Content-Type: application/json'                                                                   
+          ));                                                                                                                  
+
+    $result = curl_exec($ch);
+    $result_json_decoded = json_decode($result);
+    
+    return $this->request_result = $result_json_decoded;
+    }
+
+
+    function delete($system_realm,$account_id,$callflow_id_to_remove,$auth_token) {
+        
+    $ch = curl_init('http://' . $system_realm . ':8000/v2/accounts/' . $account_id . '/callflows' . $callflow_id_to_remove);                                                                    
+          curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");                                                                                                                                   
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                     
+          curl_setopt($ch, CURLOPT_HTTPHEADER, array(  
+          'X-Auth-Token: '.$auth_token,                                                             
+          'Content-Type: application/json'                                                                   
+          ));                                                                                                                  
+
+    $result = curl_exec($ch);
+    $result_json_decoded = json_decode($result);
+    
+    return $this->request_result = $result_json_decoded;        
+    }
+
+    
+    function update($system_realm,$account_id,$callflow_id,$data,$auth_token) {    
+                                                                                 
+    $ch = curl_init('http://' . $system_realm . ':8000/v2/accounts/' . $account_id . '/callflows' . $callflow_id);                                                                      
+          curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                    
+          curl_setopt($ch, CURLOPT_POSTFIELDS, $data);                                                                 
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                     
+          curl_setopt($ch, CURLOPT_HTTPHEADER, array(  
+          'X-Auth-Token: '.$auth_token,
+          'Content-Length: '.strlen($data),                                                              
+          'Content-Type: application/json'                                                                   
+          ));                                                                                                                  
+
+    $result = curl_exec($ch);
+    $result_json_decoded = json_decode($result);
+  
+    return $this->request_result = $result_json_decoded;
+    }
+    
+    
+}
+
+?>
